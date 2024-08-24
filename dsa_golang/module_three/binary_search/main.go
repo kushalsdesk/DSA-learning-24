0114c5fb1453c6c00f4c9de1f8bc7bc8a5e45174
kushalsdesk <kushal99mondal@gmail.com>
2024-08-24 14:33:03 +0530

    use latest util function to create array

--- dsa_golang/module_three/binary_search/main.go
+++ dsa_golang/module_three/binary_search/main.go
@@ -1,35 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"dsa_golang/utils"
+	"fmt"
+)
 
-var arrSize int
 var number int
 var flag bool
 
-func provideArray() []int {
-	counter := 0
-	fmt.Println("Enter the size of the array: ")
-	fmt.Scan(&arrSize)
-
-	fmt.Println("Remember the arrays needs to sorted for binary search. so push values in order")
-	a := make([]int, arrSize)
-	for counter < arrSize {
-		fmt.Printf("Enter the Value for position %d\n", counter)
-		fmt.Scan(&a[counter])
-		counter++
-	}
-	return a
-}
 func main() {
 
-	arr := provideArray()
+	arr := utils.ProvideArray()
 	fmt.Printf("The primary arrays is %v\n", arr)
 	fmt.Println("The number you want to find: ")
 	fmt.Scan(&number)
 	start, end := 0, len(arr)-1
 
 	for start <= end {
-		mid := (start + end) / 2
+		mid := start + (end-start)/2
 
 		if arr[mid] == number {
 			fmt.Printf("The number %d is located at index %d\n", number, mid)
--- dsa_golang/module_three/linear_search/main.go
+++ dsa_golang/module_three/linear_search/main.go
@@ -1,26 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"dsa_golang/utils"
+	"fmt"
+)
 
 var number int
-var arrSize int
-
-func provideArray() []int {
-	counter := 0
-	fmt.Println("Enter the size of the array: ")
-	fmt.Scan(&arrSize)
-
-	a := make([]int, arrSize)
-	for counter < arrSize {
-		fmt.Printf("Enter the Value for position %d\n", counter)
-		fmt.Scan(&a[counter])
-		counter++
-	}
-	return a
-}
 
 func main() {
-	a := provideArray()
+	a := utils.ProvideArray()
 	fmt.Printf("The Primary Slice is %v\n", a)
 	fmt.Println("Enter the Number to find from the array")
 	fmt.Scan(&number)
